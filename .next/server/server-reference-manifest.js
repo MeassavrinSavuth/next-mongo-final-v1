524dac6b28e6c982c170d9bc3f0772f2c5a7640b
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-09-12 14:38:12 +0700

    Update encryption key in server reference manifest

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pcSj5jRCJUZENpwUq3Ht9Ibc0FOrHm+z41pcdNRFLi0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"G5V0o70VeTqQNIyJh1fwhdinTqujAjafF6HbI+QS0R8=\"\n}"
